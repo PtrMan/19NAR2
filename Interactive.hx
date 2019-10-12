0f2c2a9ce37b1ffc871273db375002ecc78c9b0b
PtrMan <robertw89@googlemail.com>
2019-10-12 17:07:19 +0200

    Add: ded chain example

--- Interactive.hx
+++ Interactive.hx
@@ -1,8 +1,21 @@
+import sys.io.File;
+
 // interactive shell, is used like gdb
 class Interactive {
     public static function main() {
         var reasoner = new Sq2();
         reasoner.conclusionStrArr = []; // enable output logging
+
+        if (Sys.args().length > 0) {
+            var pathToLoad = Sys.args()[0];
+            var nalFileContent = File.getContent(pathToLoad);
+
+            var nalLines = nalFileContent.split("\r\n");
+            for (iNalLine in nalLines) {
+                trace(iNalLine); // debug read line
+                reasoner.input(iNalLine);
+            }
+        }
 
         while(true) {
             var inputLine: String = Sys.stdin().readLine();
